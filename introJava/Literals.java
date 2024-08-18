50b699ad6de4dfc171841319bc656ee37bb01a22
marywebby <marywebby1999@gmail.com>
2024-08-18 16:59:40 -0400

    literals

---
+++ introJava/Literals.java
@@ -0,0 +1,9 @@
+public class Literals {
+    public static void main(String[] args) {
+        int number = 42;
+        String greeting = "Hello";
+        
+        System.out.println(number);
+        System.out.println(greeting);
+    }
+}
